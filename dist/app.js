ddfc174e3d9ceb9ef71bc58d6a95f3f836bcc608
Wellington Ribeiro <wellingtonvx@gmail.com>
2020-12-12 12:32:00 -0300

    add cors

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,6 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 _dotenv2.default.config();
 
@@ -8,6 +10,18 @@ var _path = require('path');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes);
 
+const whiteList = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, );
@@ -16,6 +30,8 @@ class App {
   }
 
   middleware() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads')));
